640bee83046bf1637520dba0b089addb3f14e716
pistachio22 <3400109226@qq,com>
2025-06-27 12:21:22 +0800

    v0.0.2

--- vite-project/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <home-component></home-component>
-</template>
-
-<script lang="ts" setup>
-import HomeComponent from "../components/HomeComponent.vue";
-</script>
-
-<style lang="less" scoped></style>
