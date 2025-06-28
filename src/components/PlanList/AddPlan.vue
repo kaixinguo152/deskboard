3e7b8e05bf91c29154439f5abb917c0f8139c0b0
pistachio <3400109226@qq.com>
2025-06-28 16:42:02 +0800

    planlist迭代

---
+++ src/components/PlanList/AddPlan.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <label>
+      <input type="checkbox" v-model.lazy="inputValue" />
+      <button @click="AddPlan(plan)">Add Plan</button>
+    </label>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'AddPlan',
+  props:{
+    AddPlan : Function,
+    tid: Number,
+  },
+  setup(){
+    const plan = {
+      id: tid,
+      content: inputValue,
+    }
+    return{
+      plan,
+    }
+  }
+}
+</script>
